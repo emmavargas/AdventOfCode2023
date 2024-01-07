541ed07e2b29e1df3b8b20651a83e4bb531102ed
emma <emma.vargasvasquez@gmail.com>
2024-01-07 04:23:22 -0300

    borramos una linea

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int dato;
         Scanner ingresar = new Scanner(System.in);
-
         System.out.println("Ingrese dia: ");
         //dato = ingresar.nextInt();
         dato =7;
